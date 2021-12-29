0739e03f0bbbb15152552664548bc6adfd05c805
Andrea Funtò <dihedron.dev@gmail.com>
2021-12-30 00:27:26 +0100

    Fixed reading input from STDIN

--- ginkgo/ginkgo.go
+++ ginkgo/ginkgo.go
@@ -6,13 +6,15 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 	"text/template"
 
 	"github.com/dihedron/ginkgo/ginkgo/funcs"
+	"gopkg.in/yaml.v3"
 )
 
 type Engine struct {
-	Input Input `short:"i" long:"input" description:"The input data, either as an inline JSON value or as a @file (in JSON or YAML format)." otional:"yes" env:"GINKGO_INPUT"`
+	Input *Input `short:"i" long:"input" description:"The input data, either as an inline JSON value or as a @file (in JSON or YAML format)." otional:"yes" env:"GINKGO_INPUT"`
 	// Input     string   `short:"i" long:"input" description:"The path to the input file." optional:"yes" env:"GINKGO_INPUT"`
 	// Format    string   `short:"f" long:"format" description:"The format of the input data, if provided via STDIN." choice:"yaml" choice:"json" default:"json" optional:"yes" env:"GINKGO_FORMAT"`
 	Template  string   `short:"m" long:"main" description:"The name of the main template file." required:"yes" env:"GINKGO_TEMPLATE"`
@@ -36,9 +38,18 @@ func (cmd *Engine) Execute() error {
 		if err != nil {
 			return fmt.Errorf("error reading input data from STDIN: %w", err)
 		}
-		cmd.Input = make(map[string]interface{})
-		if err = json.Unmarshal(input, &cmd.Input); err != nil {
-			return fmt.Errorf("error unmarshalling JSON input: %w", err)
+		m := Input(make(map[string]interface{}))
+		cmd.Input = &m
+		if strings.HasPrefix(strings.TrimLeft(string(input), " \n\r"), "---") {
+			if err = yaml.Unmarshal(input, &cmd.Input); err != nil {
+				return fmt.Errorf("error unmarshalling YAML input: %w", err)
+			}
+		} else if strings.HasPrefix(strings.TrimLeft(string(input), " \n\r"), "{") {
+			if err = json.Unmarshal(input, &cmd.Input); err != nil {
+				return fmt.Errorf("error unmarshalling JSON input: %w", err)
+			}
+		} else {
+			return fmt.Errorf("unrecognisable input format on STDIN")
 		}
 	}
 	// prepare output stream
@@ -57,7 +68,7 @@ func (cmd *Engine) Execute() error {
 		output = os.Stdout
 	}
 	// execute the template
-	if err := templates.ExecuteTemplate(output, cmd.Template, map[string]interface{}(cmd.Input)); err != nil {
+	if err := templates.ExecuteTemplate(output, cmd.Template, map[string]interface{}(*cmd.Input)); err != nil {
 		fmt.Fprintf(os.Stderr, "error applying variables to template: %v\n", err)
 		return err
 	}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/dihedron/ginkgo/ginkgo"
@@ -12,10 +13,12 @@ func main() {
 
 	parser := flags.NewParser(&ginkgo, flags.Default)
 	if _, err := parser.Parse(); err != nil {
+		fmt.Fprintf(os.Stderr, "parsing error: %v\n", err)
 		os.Exit(1)
 	}
 	err := ginkgo.Execute()
 	if err != nil {
+		fmt.Fprintf(os.Stderr, "execution error: %v\n", err)
 		os.Exit(1)
 	}
 }
