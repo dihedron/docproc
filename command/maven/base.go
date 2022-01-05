f5f000863f2bce9257bb1927cf1cf84a0553ae8d
Andrea Funtò <dihedron.dev@gmail.com>
2022-01-05 21:26:44 +0100

    Added handling of GAV to maven info

--- command/maven/base.go
+++ command/maven/base.go
@@ -6,5 +6,5 @@ import "github.com/dihedron/mason/command/base"
 type Command struct {
 	base.Command
 	// Input is the optional input value, either as a file (prepend with @) or as an inline value (start with --- with)
-	POM *POM `short:"p" long:"pom" description:"The POM to process, either as an inline XML value or as a @file." otional:"yes" env:"MASON_INPUT"`
+	POM *POM `short:"p" long:"pom" description:"The POM to process, either as an inline XML value or as a @file." required:"yes" env:"MASON_INPUT"`
 }
--- command/maven/maven_info.go
+++ command/maven/maven_info.go
@@ -2,19 +2,36 @@ package maven
 
 import (
 	_ "embed"
+	"fmt"
 	"html/template"
 	"os"
 )
 
 type Info struct {
 	Command
+	GroupId    bool `short:"g" long:"group-id" description:"Print the group id." optional:"yes"`
+	ArtifactId bool `short:"a" long:"artifact-id" description:"Print the artifact id." optional:"yes"`
+	Version    bool `short:"v" long:"version" description:"Print the version." optional:"yes"`
 }
 
 //go:embed templates/simple.tpl
 var simple string
 
 func (cmd *Info) Execute(args []string) error {
-	t := template.Must(template.New("").Parse(simple))
-	t.Execute(os.Stdout, cmd.POM)
+
+	if (cmd.GroupId && cmd.ArtifactId) || (cmd.GroupId && cmd.Version) || (cmd.ArtifactId && cmd.Version) {
+		return fmt.Errorf("only one of Group ID (value: %t), Artifact ID (value: %t) and Version (value: %t) can be requested at once", cmd.GroupId, cmd.ArtifactId, cmd.Version)
+	}
+
+	if cmd.GroupId {
+		fmt.Printf("%s\n", cmd.POM.GroupId.Text)
+	} else if cmd.ArtifactId {
+		fmt.Printf("%s\n", cmd.POM.ArtifactId.Text)
+	} else if cmd.Version {
+		fmt.Printf("%s\n", cmd.POM.Version.Text)
+	} else {
+		t := template.Must(template.New("").Parse(simple))
+		t.Execute(os.Stdout, cmd.POM)
+	}
 	return nil
 }
